07dbfb914942173ec9ace0183495126a919ac759
AlexisJSG <94618215+AlexisJSG@users.noreply.github.com>
2025-02-27 18:17:10 -0600

    Add files via upload

--- login.py
+++ login.py
@@ -10,7 +10,7 @@ def signin():
     if usuario=='admin' and contra=='1234':
         root.destroy()
         menu_principal.mostrar_menuPrincpal()
-        
+
     elif usuario!='admin' and contra!='1234':
         messagebox.showerror("Error", "Usuario y Contraseña incorrecto")
 
@@ -20,34 +20,32 @@ def signin():
     elif contra!='1234':
         messagebox.showerror("Error","Contraseña Incorrecta")
 
-def mostrar_login(): 
+def mostrar_login():
 
     global root,user,contraseña
 
     root=Tk()
-    root.title('Login')
+    root.title('Frewed Commerce')
+    root.iconbitmap("imagenes/logo.ico")
     root.geometry('800x500+250+100')
     root.configure(bg="#E0E0E0")
     root.resizable(False,False)
 
-    encabezado=Label(root,text='Sistema Optifood',fg='black',bg='#E0E0E0',font=('Arial',30,'bold'))
-    encabezado.place(x=410,y=10)
+    encabezado=Label(root,text='FREWED COMMERCE',fg='black',bg='#E0E0E0',font=('Arial',28,'bold'))
+    encabezado.place(x=375,y=25)
 
     frame1=Frame(root,width=350,height=800,bg='#B0B0B0')
     frame1.place(x=0,y=0)
 
     img= PhotoImage(file='imagenes/icon-login1.png')
-    Label(frame1,image=img,bg='#B0B0B0').place(x=50,y=120)
+    Label(frame1,image=img,bg='#B0B0B0').place(x=50,y=115)
 
-    img_logo=Image.open('imagenes/logo.jpg').resize((100,100))
-    logo= ImageTk.PhotoImage(img_logo)
-    Label(frame1,image=logo,bg='#B0B0B0').place(x=0,y=0)
 
-    frame=Frame(root,width=350,height=350,bg='#D0D0D0')
-    frame.place(x=400,y=70)
+    frame=Frame(root,width=350,height=305,bg='#D0D0D0')
+    frame.place(x=400,y=100)
 
-    heading=Label(frame,text="Ingresa al sistema",fg='black',bg='#D0D0D0',font=('Arial',20,'bold'))
-    heading.place(x=50,y=5)
+    heading=Label(frame,text="INICIO DE SESIÓN",fg='black',bg='#D0D0D0',font=('Arial',20,'bold'))
+    heading.place(x=50,y=25)
 
     def on_enter(e):
         user.delete(0, 'end')
@@ -59,7 +57,7 @@ def mostrar_login():
 
     user=Entry(frame,width=30,fg='black',border=2,bg='white',font=('Arial',11))
     user.place(x=50,y=100)
-    user.insert(0,'Usuario')
+    user.insert(0,'USUARIO')
     user.bind('<FocusIn>', on_enter)
     user.bind('<FocusOut>', on_leave)
 
@@ -72,11 +70,11 @@ def mostrar_login():
             contraseña.insert(0,'Contraseña')
 
     contraseña=Entry(frame,width=30,fg='black',border=2,bg='white',font=('Arial',11),show='*')
-    contraseña.place(x=50,y=180)
+    contraseña.place(x=50,y=165)
     contraseña.insert(0,'Contraseña')
     contraseña.bind('<FocusIn>', on_enter)
     contraseña.bind('<FocusOut>', on_leave)
 
-    Button(frame,width=30,pady=7,text='Ingresar',bg='midnightblue',fg='white',font=('Arial', 12),border=5,command=signin).place(x=35,y=250)
+    Button(frame,width=30,pady=7,text='INGRESAR',bg='midnightblue',fg='white',font=('Arial', 12),border=5,command=signin).place(x=35,y=225)
 
     root.mainloop()
