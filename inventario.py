0e689d77e5c2ef1832201778c47b8d942fdc7e9b
AlexisJSG <94618215+AlexisJSG@users.noreply.github.com>
2025-02-28 00:58:07 -0600

    Add files via upload

--- inventario.py
+++ inventario.py
@@ -43,46 +43,46 @@ class Ventana(Frame):
         self.frame_dos.columnconfigure(0, weight=1)
         self.frame_dos.rowconfigure(0, weight=1)
 
-        Button(self.frame_titulo, text='REGRESAR', font = ('Arial', 9, 'bold'), command=salir_invetario, fg='black', bg = 'deep sky blue', width=20, bd=3).grid(column=0, row=0, pady=5)
-        Label(self.frame_titulo, text= 'Inventario', bg='#FFD961', fg='black', font=('Kaufmann BT', 25, 'bold')).grid(columnspan=5, column=0, row=1)
+        Button(self.frame_titulo, text='REGRESAR', font = ('Kaufmann BT', 12, 'bold'), command=salir_invetario, fg='white', bg = 'dimgray', width=15, bd=3).grid(column=0, row=1, padx=(0,250),pady=(20,0))
+        Label(self.frame_titulo, text= 'INVENTARIO', bg='#FFD961', fg='black', font=('Kaufmann BT', 25, 'bold')).grid(columnspan=5, column=0, row=1,pady=(20,5))
 
-        Label(self.frame_uno, text= 'Opciones', bg='black', fg='white', font=('Kaufmann BT', 13, 'bold')).grid(column=2, row=0)
+        Label(self.frame_uno, text= 'OPCIONES', bg='#FFD961', fg='black', font=('Rockwell', 15, 'bold')).grid(column=2, row=0)
         Button(self.frame_uno, text='REFRESCAR', font = ('Arial', 9, 'bold'), command=self.actualizar_tabla, fg='black', bg = 'deep sky blue', width=20, bd=3).grid(column=2, row=1, pady=5)
 
-        Label(self.frame_uno, text= 'Datos', bg='black', fg='white', font=('Kaufmann BT', 13, 'bold')).grid(columnspan=2, column=0, row=0, pady=5)
+        Label(self.frame_uno, text= 'DATOS', bg='#FFD961', fg='black', font=('Rockwell', 15, 'bold')).grid(columnspan=3, column=0, row=0, pady=5)
         Label(self.frame_uno, text= 'Nombre', bg='#FFD961', fg='black', font=('Rockwell', 13, 'bold')).grid(column=0, row=1, pady=5)
-        Label(self.frame_uno, text= 'CantidadI', bg='#FFD961', fg='black', font=('Rockwell', 13, 'bold')).grid(column=0, row=2, pady=5)
+        Label(self.frame_uno, text= 'Cantidad', bg='#FFD961', fg='black', font=('Rockwell', 13, 'bold')).grid(column=0, row=2, pady=5)
         Label(self.frame_uno, text= 'Unidad Medida', bg='#FFD961', fg='black', font=('Rockwell', 13, 'bold')).grid(column=0, row=3, pady=5)
         Label(self.frame_uno, text= 'Categoría', bg='#FFD961', fg='black', font=('Rockwell', 13, 'bold')).grid(column=0, row=4, pady=5)
-        Label(self.frame_uno, text= 'Proveedor', bg='#FFD961', fg='black', font=('Rockwell', 13, 'bold')).grid(column=0, row=5, pady=5)
+        Label(self.frame_uno, text= 'Proveedor', bg='#FFD961', fg='black', font=('Rockwell', 13, 'bold')).grid(column=0, row=5, pady=(5,15))
 
-        Entry(self.frame_uno, textvariable=self.nombre , font=('Comic Sans MS', 12), highlightbackground="black", highlightthickness=1).grid(column=1, row=1)
-        Entry(self.frame_uno, textvariable=self.cantidadI , font=('Comic Sans MS', 12), highlightbackground="black", highlightthickness=1).grid(column=1, row=2)
-        Entry(self.frame_uno, textvariable=self.proveedor , font=('Comic Sans MS', 12), highlightbackground="black", highlightthickness=1).grid(column=1, row=5)
+        Entry(self.frame_uno, textvariable=self.nombre , font=('Comic Sans MS', 12), highlightbackground="black", highlightthickness=1).grid(column=1, row=1, padx=(15,0))
+        Entry(self.frame_uno, textvariable=self.cantidadI , font=('Comic Sans MS', 12), highlightbackground="black", highlightthickness=1).grid(column=1, row=2,padx=(15,0))
+        Entry(self.frame_uno, textvariable=self.proveedor , font=('Comic Sans MS', 12), highlightbackground="black", highlightthickness=1).grid(column=1, row=5,padx=(15,0),pady=(5,15))
         # Entry(self.frame_uno, textvariable=self.correo , font=('Comic Sans MS', 12), highlightbackground="deep sky blue", highlightthickness=5).grid(column=1, row=3)
         # Entry(self.frame_uno, textvariable=self.telefono , font=('Comic Sans MS', 12), highlightbackground="deep sky blue", highlightthickness=5).grid(column=1, row=4)
 
         opciones_unidadMed = ["kg", "lt", "pza(s)", "paquete(s)"]
         self.combo_unidadMed = ttk.Combobox(self.frame_uno, values=opciones_unidadMed, font=('Comic Sans MS', 12))
         self.combo_unidadMed.set(opciones_unidadMed[0])
-        self.combo_unidadMed.grid(row=3, column=1)
+        self.combo_unidadMed.grid(row=3, column=1,padx=(15,0))
         self.unidadMed = self.combo_unidadMed.get()
 
         opciones_cat = ["Vegetales", "Carnes", "Variado", "Limpieza"]
         self.combo_cat = ttk.Combobox(self.frame_uno, values=opciones_cat, font=('Comic Sans MS', 12))
         self.combo_cat.set(opciones_cat[0])
-        self.combo_cat.grid(row=4, column=1)
+        self.combo_cat.grid(row=4, column=1,padx=(15,0))
         self.categoria = self.combo_cat.get()
 
         Button(self.frame_uno, text='AÑADIR A INVENTARIO', font= ('Arial', 9, 'bold'), bg= '#54FE2A', width=20, bd=3, command=self.agregar_datos).grid(column=2, row=2, pady=5, padx=5)
         Button(self.frame_uno, text='LIMPIAR CAMPOS', font= ('Arial', 9, 'bold'), bg= '#C3C3C3', width=20, bd=3, command=self.limpiar_campos).grid(column=2, row=3, pady=5, padx=5)
         Button(self.frame_uno, text='ACTUALIZAR DATOS', font= ('Arial', 9, 'bold'), bg= '#D133E7', width=20, bd=3, command=self.actualizar_datos).grid(column=2, row=4, pady=5, padx=5)
-        Button(self.frame_uno, text='EXPORTAR A EXCEL', font= ('Arial', 9, 'bold'), bg= '#03BF00', width=20, bd=3, command=self.guardar_datos).grid(column=2, row=5, pady=5, padx=5)
+        Button(self.frame_uno, text='EXPORTAR A EXCEL', font= ('Arial', 9, 'bold'), bg= '#03BF00', width=20, bd=3, command=self.guardar_datos).grid(column=2, row=5, pady=(5,15), padx=5)
 
         estilo_tabla = ttk.Style()
         estilo_tabla.configure("Treeview", font= ('Helvetica', 10, 'bold'), foreground='black', background='#FFE593')
         estilo_tabla.map('Treeview', background=[('selected', 'deep sky blue')], foreground=[('selected','black')] )
-        estilo_tabla.configure('Heading', background='white', foreground='deep sky blue', padding=3, font=('Arial', 10, 'bold'))
+        estilo_tabla.configure('Heading', background='white', foreground='black', padding=3, font=('Arial', 10, 'bold'))
 
         self.tabla = ttk.Treeview(self.frame_dos)
         self.tabla.grid(column=0, row=0, sticky='nsew')
@@ -99,9 +99,9 @@ class Ventana(Frame):
         self.tabla.column('Categoria', minwidth=100, width=105, anchor='center')
         self.tabla.column('Proveedor', minwidth=100, width=105, anchor='center')
 
-        self.tabla.heading('#0', text="Nombre", anchor='center')
-        self.tabla.heading('CantidadI', text="CantidadI", anchor='center')
-        self.tabla.heading('UnidadMed', text="UnidadMed", anchor='center')
+        self.tabla.heading('#0', text="    Nombre", anchor='center')
+        self.tabla.heading('CantidadI', text="Cantidad", anchor='center')
+        self.tabla.heading('UnidadMed', text="Unidad Medida", anchor='center')
         self.tabla.heading('Categoria', text="Categoria", anchor='center')
         self.tabla.heading('Proveedor', text="Proveedor", anchor='center')
 
@@ -112,9 +112,9 @@ class Ventana(Frame):
         item = self.tabla.focus()
         self.data = self.tabla.item(item)
         self.nombre.set(self.data['text'])
-        self.cantidad.set(self.data['values'][0])
-        # self.unidadMed.set(self.data['values'][1])
-        # self.categoria.set(self.data['values'][2])
+        self.cantidadI.set(self.data['values'][0])
+        self.unidadMed.set(self.data['values'][1])
+        self.categoria.set(self.data['values'][2])
         self.combo_unidadMed.set(self.data['values'][1])
         self.combo_cat.set(self.data['values'][2])
         self.proveedor.set(self.data['values'][3])
@@ -130,7 +130,7 @@ class Ventana(Frame):
     
     def agregar_datos(self):
         nombre = self.nombre.get()
-        cantidadI = self.cantidad.get()
+        cantidadI = self.cantidadI.get()
         unidadMed = self.combo_unidadMed.get()    
         categoria = self.combo_cat.get()
         proveedor = self.proveedor.get()
@@ -207,7 +207,8 @@ class Ventana(Frame):
 def mostrar_inventario():
     global ventana
     ventana = Tk()
-    ventana.title('Inventario')
+    ventana.title('Frewed Commerce')
+    ventana.iconbitmap("imagenes/logo.ico")
     ventana.minsize(height=400, width=600)
     ventana.geometry('800x500+250+100')
     # ventana.call('wm', 'iconphoto', ventana._w, PhotoImage(file='logo.png'))
